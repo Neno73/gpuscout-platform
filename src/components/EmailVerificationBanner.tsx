4344d7e2ecc4bd9286c7740ca675760c2f3064f8
Neno <nenad@sols.mk>
2025-07-26 08:52:47 +0200

    WIP: TASK-001

--- src/components/EmailVerificationBanner.tsx
+++ src/components/EmailVerificationBanner.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 interface EmailVerificationBannerProps {
   user: User;
-  onResendVerification: () => Promise<void>;
+  onResendVerification?: () => Promise<void>;
   onDismiss?: () => void;
 }
 
