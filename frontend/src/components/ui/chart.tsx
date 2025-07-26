208a1ccc9e41517efabf4299160421d5abaf7fc1
Neno <nenad@sols.mk>
2025-07-26 15:10:42 +0200

    WIP: development

--- frontend/src/components/ui/chart.tsx
+++ frontend/src/components/ui/chart.tsx
@@ -258,7 +258,7 @@ const ChartTooltipContent = React.forwardRef<
                   key={item.dataKey}
                   className={cn(
                     "flex w-full items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground",
-                    indicator === "dot" && "items-center"
+                    indicator === "dot" ? "items-center" : ""
                   )}
                 >
                   <div
